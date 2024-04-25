62d454b6e9dbea790643316c8b1cb2679dacafec
hjunior29 <helderjuniorsilvalima@gmail.com>
2024-04-25 02:29:05 -0300

    feat: middleware with helpers

--- go.mod
+++ go.mod
@@ -3,16 +3,17 @@ module github.com/flambra/account
 go 1.20
 
 require (
-	github.com/flambra/helpers v0.0.0-20240423031645-d5101931935f
+	github.com/flambra/helpers v0.0.0-20240425031119-d6550bcc571e
 	github.com/gofiber/fiber/v2 v2.52.4
 	github.com/joho/godotenv v1.5.1
-	golang.org/x/crypto v0.14.0
+	golang.org/x/crypto v0.22.0
 	gorm.io/driver/postgres v1.5.7
 	gorm.io/gorm v1.25.9
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -27,6 +28,6 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,52 +1,18 @@
 package middleware
 
 import (
-	"io"
-	"net/http"
-	"os"
-
+	"github.com/flambra/helpers/access"
+	"github.com/flambra/helpers/http"
 	"github.com/gofiber/fiber/v2"
 )
 
-// Auth creates a middleware to verify the authentication token.
+// Auth creates a middleware to validate the authentication token.
 func Auth(c *fiber.Ctx) error {
 	token := c.Get("Authorization")
-	if token == "" {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"error": "Authorization token not provided",
-		})
-	}
 
-	authURL := os.Getenv("AUTH_URL")
-	if authURL == "" {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "Authentication service URL not configured",
-		})
-	}
-
-	authReq, err := http.NewRequest("GET", authURL + "/client/verify", nil)
+	err := access.ValidateToken(token)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "Failed to create request to authentication service",
-		})
-	}
-
-	authReq.Header.Set("Authorization", token)
-	client := &http.Client{}
-	res, err := client.Do(authReq)
-	if err != nil {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"error": "Failed to verify token",
-		})
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != http.StatusOK {
-		body, _ := io.ReadAll(res.Body)
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"error": "Invalid token",
-			"body":  string(body),
-		})
+		return http.UnauthorizedResponse(c, err.Error())
 	}
 
 	return c.Next()
