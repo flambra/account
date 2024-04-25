e0537f73f241af65edaaf6c082beb1fd4350304c
luiseasyc <luis.vieira@easyc.com.br>
2024-04-25 14:28:04 -0300

    feat: error fix

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,16 +1,16 @@
 package middleware
 
 import (
-	"github.com/flambra/helpers/access"
+	token "github.com/flambra/helpers/access"
 	"github.com/flambra/helpers/http"
 	"github.com/gofiber/fiber/v2"
 )
 
 // Auth creates a middleware to validate the authentication token.
 func Auth(c *fiber.Ctx) error {
-	token := c.Get("Authorization")
+	tokenAuthorizer := c.Get("Authorization")
 
-	err := access.ValidateToken(token)
+	err := token.ValidateToken(tokenAuthorizer)
 	if err != nil {
 		return http.UnauthorizedResponse(c, err.Error())
 	}
