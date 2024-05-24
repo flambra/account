68986cd11b86645a2c75787708da1ed0c993fc05
hjunior29 <helderjuniorsilvalima@gmail.com>
2024-05-24 03:07:58 -0300

    feat: auth middleware bypass

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"os"
+
 	"github.com/flambra/helpers/hAuth"
 	"github.com/flambra/helpers/hResp"
 	"github.com/gofiber/fiber/v2"
@@ -8,6 +10,10 @@ import (
 
 // Auth creates a middleware to hValidate the authentication token.
 func Auth(c *fiber.Ctx) error {
+	if os.Getenv("AUTH_MIDDLEWARE") == "disable" {
+		return c.Next()
+	}
+
 	token := c.Get("Authorization")
 
 	err := hAuth.ValidateToken(token)
